05cf8966ae7eb12ff74621efe1383fb113acbbc7
Eduardo Borges <euduardoborges@gmail.com>
2022-04-06 21:35:11 -0300

    chore: fim do dojo

--- src/index.ts
+++ src/index.ts
@@ -79,9 +79,32 @@ export const generateDiamond = (letter: string) => {
     "Z"
   ];
 
-  const deepth = letters.findIndex((l) => l === letter);
+  // const deep = letter.charCodeAt(0) - 'A'.charCodeAt(0);
 
-  const array = Array.from(new Array(deepth)).fill();
+  const deepth = letters.findIndex((l) => l === letter) + 1;
+
+  const genSpaces = (n: number) => {
+    let spaces = "";
+    for (let index = 0; index < n; index++) {
+      spaces += "#";
+    }
+    return Array.from(new Array(n)).fill("#");
+  };
+
+  const array = Array.from(new Array(2 * deepth - 1)).map((_, i) => {
+    return genSpaces(deepth + 1);
+  });
+
+  log(array);
+
+  const center = Math.floor(deepth / 2) + (deepth % 2);
+  for (let j = 0; j < 2 * deepth - 1; j++) {
+    for (let i = 0; i < deepth; i++) {
+      array[j][center + i] = letters[i];
+      array[j][center - i] = letters[i];
+      log(array[j].join(""));
+    }
+  }
 
   if (letter === "A") {
     return "A";
@@ -89,7 +112,6 @@ export const generateDiamond = (letter: string) => {
 
   if (letter === "B") {
     return `
-
  A
 
 B B
@@ -100,4 +122,4 @@ B B
   }
 };
 
-generateDiamond("A");
+generateDiamond("C");
